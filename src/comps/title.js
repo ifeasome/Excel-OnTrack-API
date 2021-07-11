9c7e6d3d73134ce835bd726a9ab4af5ba70c5ae8
Somelina Obiechina <ifeasome@gmail.com>
2021-07-11 16:52:05 -0500

    Frontend Display

---
+++ src/comps/title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Excel OnTrack</h1>
+      <h2>Excel File</h2>
+      <p>Upload your excel file</p>
+    </div>
+  )
+}
+
+export default Title;
